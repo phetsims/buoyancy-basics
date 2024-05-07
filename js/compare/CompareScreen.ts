53343204acbe8dbc93517a4d0d6bf53025516f43
Sam Reid <reids@colorado.edu>
2024-05-07 09:04:26 -0600

    Add keyboard help, see https://github.com/phetsims/density-buoyancy-common/issues/102

--- js/compare/CompareScreen.ts
+++ js/compare/CompareScreen.ts
@@ -14,6 +14,7 @@ import BuoyancyBasicsCompareModel from '../../../density-buoyancy-common/js/buoy
 import BuoyancyBasicsCompareScreenView from '../../../density-buoyancy-common/js/buoyancy-basics/view/BuoyancyBasicsCompareScreenView.js';
 import BuoyancyBasicsStrings from '../BuoyancyBasicsStrings.js';
 import ScreenIcon from '../../../joist/js/ScreenIcon.js';
+import DensityBuoyancyCommonKeyboardHelpNode from '../../../density-buoyancy-common/js/common/view/DensityBuoyancyCommonKeyboardHelpNode.js';
 
 export default class CompareScreen extends Screen<BuoyancyBasicsCompareModel, BuoyancyBasicsCompareScreenView> {
   public constructor( tandem: Tandem ) {
@@ -31,7 +32,8 @@ export default class CompareScreen extends Screen<BuoyancyBasicsCompareModel, Bu
           maxIconWidthProportion: 1,
           maxIconHeightProportion: 1
         } ),
-        tandem: tandem
+        tandem: tandem,
+        createKeyboardHelpNode: () => new DensityBuoyancyCommonKeyboardHelpNode( true, false )
       }
     );
   }
--- js/explore/ExploreScreen.ts
+++ js/explore/ExploreScreen.ts
@@ -14,6 +14,7 @@ import BuoyancyBasicsExploreModel from '../../../density-buoyancy-common/js/buoy
 import BuoyancyBasicsExploreScreenView from '../../../density-buoyancy-common/js/buoyancy-basics/view/BuoyancyBasicsExploreScreenView.js';
 import BuoyancyBasicsStrings from '../BuoyancyBasicsStrings.js';
 import ScreenIcon from '../../../joist/js/ScreenIcon.js';
+import DensityBuoyancyCommonKeyboardHelpNode from '../../../density-buoyancy-common/js/common/view/DensityBuoyancyCommonKeyboardHelpNode.js';
 
 export default class ExploreScreen extends Screen<BuoyancyBasicsExploreModel, BuoyancyBasicsExploreScreenView> {
   public constructor( tandem: Tandem ) {
@@ -31,7 +32,8 @@ export default class ExploreScreen extends Screen<BuoyancyBasicsExploreModel, Bu
           maxIconWidthProportion: 1,
           maxIconHeightProportion: 1
         } ),
-        tandem: tandem
+        tandem: tandem,
+        createKeyboardHelpNode: () => new DensityBuoyancyCommonKeyboardHelpNode( true, true )
       }
     );
   }
