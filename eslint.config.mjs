5f6bab15efb0c9a3d77137e3fe7ea130e2eaa9b0
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:59 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for buoyancy-basics.
+ * ESLint configuration for buoyancy-basics.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
