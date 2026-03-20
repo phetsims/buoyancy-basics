2d380fcc56f039555890703f1cd787b6d130adf0
phet-dev <phetadmin@gmail.com>
2026-03-20 13:50:31 -0600

    grunt update  from daily grunt work

--- js/BuoyancyBasicsStrings.ts
+++ js/BuoyancyBasicsStrings.ts
@@ -22,4 +22,6 @@ type StringsType = {
 
 const BuoyancyBasicsStrings = getStringModule( 'BUOYANCY_BASICS' ) as StringsType;
 
+buoyancyBasics.register( 'BuoyancyBasicsStrings', BuoyancyBasicsStrings );
+
 export default BuoyancyBasicsStrings;
